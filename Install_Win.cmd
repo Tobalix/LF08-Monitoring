0565e1cb6b457275cf48668997bfe5ed3688332a
Tobalix <115449945+Tobalix@users.noreply.github.com>
2023-12-07 17:02:15 +0100

    Update Install_Win.cmd

--- Install_Win.cmd
+++ Install_Win.cmd
@@ -1,1 +1,20 @@
+::
+::
+::
 
+
+:: Start Installation Windows
+ECHO on
+ECHO "Installation wird begonnen"
+:: Erstelen Orderner Struktur
+cd %windir%
+cd ../
+cd Program Files
+
+MD ITECH_MONITOR
+cd ITECH_MONITOR
+MD LOG
+MD SOURCE
+MD CONFIG
+
+:: Installirt Programm von Git
