909791b68ef622f9d1eadc5fa7381a22f65c85bc
Mike <mike.jessen70@gmail.com>
2023-12-12 11:21:07 +0100

    edit user path in batch

--- ordner test erstellen.bat
+++ ordner test erstellen.bat
@@ -1,6 +1,6 @@
-mkdir C:\Users\julia.salikov\Test
+mkdir C:\Users\%USER%\Test
 
-cd C:\Users\julia.salikov\Test\
+cd C:\Users\%USER%\Test\
 
 git clone https://github.com/Tobalix/LF08-Monitoring.git
 
