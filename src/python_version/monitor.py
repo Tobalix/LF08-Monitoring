36aa14512c1c7ac40d63d5960ff18e3997e055b7
Mike <mike.jessen70@gmail.com>
2023-12-12 11:27:03 +0100

    renamed variables

--- src/python_version/monitor.py
+++ src/python_version/monitor.py
@@ -6,32 +6,37 @@ import time
 
 print(psutil.virtual_memory().percent)
 
+
 def cpu_log(PATH):
-    CPU_USE = psutil.cpu_percent()
+    cpu_use = psutil.cpu_percent()
     logging.basicConfig(filename="%sCPU.log" % PATH, encoding='utf-8', level=logging.DEBUG)
-    #logging.debug('This message should go to the log file')
+    # logging.debug('This message should go to the log file')
     dt_string = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    if CPU_USE >= 80:
-        logging.warning("%s CPU: %s" %( dt_string, CPU_USE) )
+    if cpu_use >= 80:
+        logging.warning("%s CPU: %s" % (dt_string, cpu_use))
     else:
-        logging.info("%s CPU: %s" %( dt_string, CPU_USE) )
-    #logging.warning('And this, too')
-    #logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+        logging.info("%s CPU: %s" % (dt_string, cpu_use))
+    # logging.warning('And this, too')
+    # logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+
 x = 0
+
+
 def ram_log(PATH):
-    RAM_USE = psutil.virtual_memory().percent
+    ram_use = psutil.virtual_memory().percent
     logging.basicConfig(filename="%sRAM.log" % PATH, encoding='utf-8', level=logging.DEBUG)
-    #logging.debug('This message should go to the log file')
+    # logging.debug('This message should go to the log file')
     dt_string = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    if RAM_USE >= 80:
-        logging.warning("%s RAM: %s" %( dt_string, RAM_USE) )
+    if ram_use >= 80:
+        logging.warning("%s RAM: %s" % (dt_string, ram_use))
     else:
-        logging.info("%s RAM: %s" %( dt_string, RAM_USE) )
+        logging.info("%s RAM: %s" % (dt_string, ram_use))
 
 
-while(x < 10):
+while (x < 10):
     cpu_log("")
     ram_log("")
     time.sleep(2)
-    x = x+1
+    x = x + 1
     print(x)
