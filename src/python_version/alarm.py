8661a119ab5d3ce8a28a999e5df736f589eadefb
Mike <mike.jessen70@gmail.com>
2023-12-13 12:48:26 +0100

    edited subject and content in alarm.py

--- src/python_version/alarm.py
+++ src/python_version/alarm.py
@@ -13,33 +13,31 @@ def send_mail():
     password = "bagel-footman-prevent"
 
     message = MIMEMultipart("alternative")
-    message["Subject"] = "multipart test"
+    message["Subject"] = "Monitoring Alarm"
     message["From"] = sender_email
     message["To"] = receiver_email
 
     html = """\
     <html>
       <body>
-        <p>Hi,<br>
-           How are you?<br>
-           <a href="http://www.realpython.com">Real Python</a> 
-           has many great tutorials.
+        <p>Hello,<br>
+           Your alarm has triggered for<br>
+           <a href="http://www.realpython.com">ALARM</a> 
+           please refer to this link to see the alert.
         </p>
       </body>
     </html>
     """
     part = MIMEText(html, "html")
-
     message.attach(part)
 
     context = ssl.create_default_context()
 
-    # Initialize server to None
     server = None
 
     # Try to log in to the server and send email
     try:
-        server = smtplib.SMTP(smtp_server, port, timeout=10)  # Increase the timeout value
+        server = smtplib.SMTP(smtp_server, port, timeout=10)
         server.starttls(context=context)  # Secure the connection
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message.as_string())
