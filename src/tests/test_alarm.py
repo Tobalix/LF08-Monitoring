2ab4ae2762385d14f0f10eeae57b618185ea958a
Ceeleci <122790233+Ceeleci@users.noreply.github.com>
2023-12-20 12:03:05 +0100

    Delete src directory

--- src/tests/test_alarm.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class TestAlarm(TestCase):
-    def test_send_mail(self):
-        self.assertTrue(True)
--- src/tests/test_monitor.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class TestMonitoring(unittest.TestCase):
- def setUp(self):
-     self.assertTrue(True)
