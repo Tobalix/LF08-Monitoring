7a7ed8d2a53560a3bcea77ad3be876517e7a130f
MikeMiau <mike.jessen70@gmail.com>
2024-01-08 13:19:19 +0100

    Alarm kinda finished

--- src/python_version/alarm.py
+++ src/python_version/alarm.py
@@ -15,14 +15,13 @@ from email import encoders
 # from src.log_examples.monitor import cpu_log, ram_log, disk_log
 
 
-def send_mail(timenow, log_file_path):
+def send_mail(timenow, log_file_path, alarm_type):
     # Setup
     smtp_server = "smtp.mail.de"
     port = 25
     sender_email = "IT.Monitor@mail.de"
     receiver_email = "IT.Monitor@mail.de"
     password = "bagel-footman-prevent"
-    alarm_type = "CPU"
 
     message = MIMEMultipart("alternative")
     message["Subject"] = "Monitoring Alarm"
@@ -31,7 +30,6 @@ def send_mail(timenow, log_file_path):
 
     with open(log_file_path, "r") as log_file:
         log_content = log_file.readlines()[-1]
-
 
     html = f"""\
     <html>
@@ -48,7 +46,7 @@ def send_mail(timenow, log_file_path):
     message.attach(part)
 
     # Attachment of logs
-    file_path = "../../logs/2024-01-08_CPU.log"  # Replace with the actual path to your file
+    file_path = f"{log_file_path}"  # Replace with the actual path to your file
     attachment = open(file_path, "rb")
     mime_type = "text/plain"
     part = MIMEBase(*mime_type.split("/"))
@@ -80,13 +78,15 @@ def send_mail(timenow, log_file_path):
 
 def alarm():
     x = 0
-    count = 0
+    alarm_type = "CPU"
     now = datetime.now()
     date_today = now.strftime("%Y-%m-%d_")
     while x == x:
         cpu_log_file_path = f"../../logs/{date_today}CPU.log"
         cpu_log = open(f'{cpu_log_file_path}', 'r')
         cpu_lines = cpu_log.readlines()
+
+        count = 0
         for line in cpu_lines:
             count += 1
             print("L{}: {}".format(count, line.strip()))
@@ -94,24 +94,39 @@ def alarm():
         disk_log_file_path = f"../../logs/{date_today}DISK.log"
         disk_log = open(f'{disk_log_file_path}', 'r')
         disk_lines = disk_log.readlines()
+        count = 0
         for line in disk_lines:
             count += 1
             print("Line{}: {}".format(count, line.strip()))
 
         ram_log_file_path = f"../../logs/{date_today}RAM.log"
-        ram_log = open(f'{disk_log_file_path}', 'r')
+        ram_log = open(f'{ram_log_file_path}', 'r')
         ram_lines = ram_log.readlines()
+        count = 0
         for line in ram_lines:
             count += 1
             print("Line{}: {}".format(count, line.strip()))
+
         time_now = now.strftime("%Y-%m-%d, %H:%M:%S")
 
-
-        # Strips the newline character
+        if cpu_lines[-1].split(" ")[0] == "WARNING":
+            alarm_type = "CPU"
+            send_mail(time_now, cpu_log_file_path, alarm_type)
+            time.sleep(500)
+        if disk_lines[-1].split(" ")[0] == "WARNING":
+            alarm_type = "DISK"
+            send_mail(time_now, disk_log_file_path, alarm_type)
+            time.sleep(500)
+        if ram_lines[-1].split(" ")[0] == "WARNING":
+            alarm_type = "RAM"
+            send_mail(time_now, ram_log_file_path, alarm_type)
+            time.sleep(500)
+        else:
+            print("no email sent")
+            time.sleep(5)
 
         print(x)
-        send_mail(time_now, cpu_log_file_path)
-        time.sleep(60)
+        # send_mail(time_now, cpu_log_file_path, alarm_type)
 
 
 alarm()
