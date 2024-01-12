45759059533d8a968d5897083d347f3d7acf94f6
MikeMiau <mike.jessen70@gmail.com>
2024-01-12 12:56:06 +0100

    implemented at least one "test"

--- src/python_version/main.py
+++ src/python_version/main.py
@@ -1,6 +1,6 @@
 from multiprocessing import Process
 from monitor import start
-from alarm import Alarm
+from alarm import alarm
 
 
 def monitoring(seconds):
@@ -8,7 +8,7 @@ def monitoring(seconds):
 
 
 def alarming(seconds):
-    Alarm().alarm()
+    alarm()
 
 
 p1 = Process(target=monitoring, args=[1])
--- src/tests/test_alarm.py
+++ src/tests/test_alarm.py
@@ -1,6 +1,15 @@
-from unittest import TestCase
+import unittest
+
+import unittest
 
 
-class TestAlarm(TestCase):
-    def test_alarm(self):
-        self.assertTrue(True)
+class TestAlarmFunctionality(unittest.TestCase):
+
+    def some_test(self):
+        value1 = 1
+        value2 = 2
+        self.assertTrue(value1 + value2 == 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
