11a63546212f491a55380356a1d5209230476db2
cuubezzz <78969059+cuubezzz@users.noreply.github.com>
2024-01-17 08:46:33 +0100

    latest version

--- src/python_version/graph.py
+++ src/python_version/graph.py
@@ -3,16 +3,19 @@ import numpy as np
 import psutil
 import time
 
-l = 1000
+
+# Die Datenpunkte für die X-Axis befüllen
+l = 40000
 x = range(1, l+1)
-frames = 1000
+# Leere Arrays initialisieren für die Auslastung der Komponenten
 cpu_data = []
 ram_data = []
-
+# Variablen um das scrollen den Graphen zu kontrollieren
 upper_lim = 20
 lower_lim = 0
 interval = 0
 
+# Funktion welche einen animierten Graphen darstellen lässt
 def anim_graph(title, data = [], max_point = 20, min_point = 0, pos = 1):
     plotext.title(title)
     plotext.clt()
@@ -22,25 +25,26 @@ def anim_graph(title, data = [], max_point = 20, min_point = 0, pos = 1):
     plotext.plot(x, data, fillx=True, color="blue")
     plotext.show()
     
-
+# Gibt einen die Auslastung des CPUs in Prozent
 def get_cpu():
     cpu_percent = psutil.cpu_percent()
     return cpu_percent
-
+# Gibt einen die Auslastung des RAMs in Prozent
 def get_ram():
     ram_percent = psutil.virtual_memory().percent
     return ram_percent
-
+# Nimmt eine Zahl und ein Array und fügt die Zahl dem Array hinzu
 def build_data(value, data):
     data.append(round(value))
     return data
 
 
-
+# Initialisierung der Plots
 plotext.clc()
 plotext.clt()
 plotext.subplots(1,2)
 
+# Haupt-Loop
 while True:
     plotext.subplot(1,1)
     anim_graph("CPU Graph", build_data(get_cpu(), cpu_data),upper_lim, lower_lim)
